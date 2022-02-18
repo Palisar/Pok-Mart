2a0e9c6913ca747a0f488d1f78419ff2ad740236
Paul Keating <palisar@hotmail.co.uk>
2022-02-18 19:55:37 +0000

    Added a product response type to be used for object mapping

---
+++ PokeMart.Contracts/Responses/ProductResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeMart.Contracts.Responses
+{
+    public class ProductResponse
+    {
+        public string Id { get; init; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
+
+//TODO : Add Mapster for object mapping
