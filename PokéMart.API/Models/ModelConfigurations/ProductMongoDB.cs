d844e45732620838a50b5ce4c6093453abe29b34
Paul Keating <palisar@hotmail.co.uk>
2022-02-23 23:15:43 +0000

    restructered the folder

---
+++ PokéMart.API/Models/ModelConfigurations/ProductMongoDB.cs
@@ -0,0 +1,9 @@
+﻿namespace PokéMart.API.DataAccess
+{
+    public class ProductMongoDB
+    {
+        public string connectionString { get; set; } = null!;
+        public string databaseName { get; set; } = null!;
+        public string collectionName { get; set; } = null!;
+    }
+}
--- PokéMart.API/Models/Product.cs
+++ PokéMart.API/Models/Product.cs
@@ -1,5 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations;
 
 namespace PokéMart.API.Models
@@ -7,11 +6,9 @@ namespace PokéMart.API.Models
     public class Product
     {
         [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; init; }
-        [MaxLength(200)]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
         public string Name { get; set; }
-        [MaxLength(300)]
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
