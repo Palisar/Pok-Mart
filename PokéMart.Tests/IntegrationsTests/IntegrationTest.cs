dc210fe3aec1fe86334bc93bdd528cd2d066e16e
Paul Keating <palisar@hotmail.co.uk>
2022-02-23 20:23:42 +0000

    added integration testing class

---
+++ PokéMart.Tests/IntegrationsTests/IntegrationTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PokéMart.Tests.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        private readonly HttpClient _client;
+
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Program>();
+            _client = appFactory.CreateClient();
+        }
+        
+        [Theory]
+        [InlineData("/api/product")]
+        public async Task GetAllProducts_ReturnsOk(string endpoint)
+        {
+            var response = await _client.GetAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+        }
+
+    }
+}
--- PokéMart.Tests/ProductControllerTests.cs
+++ PokéMart.Tests/ProductControllerTests.cs
@@ -3,9 +3,13 @@
     public class ProductControllerTests
     {
         [Fact]
-        public void AddProduct()
+        public void AddProduct_Then_ReturnOK200()
         {
-
+            var mock = new Mock<Product>();
+            mock.Setup(prod => prod.Id).Returns("daw4e543");
+            mock.Setup(prod => prod.Name).Returns("Thing");
+            mock.Setup(prod => prod.Description).Returns("Does stuff");
+            mock.Setup(prod => prod.Price).Returns(100);
         }
     }
 }
