c5a535d2db2c38dedd29cf65238ccfee2fd19969
Paul Keating <palisar@hotmail.co.uk>
2022-02-24 15:33:30 +0000

    Added in memory database for testing purposes

---
+++ PokéMart.API/Services/InMemoryProductService.cs
@@ -0,0 +1,70 @@
+﻿namespace PokéMart.API.Services
+{
+    public class InMemoryProductService : IProductService
+    {
+        private readonly List<Product> products = new();
+        public InMemoryProductService()
+        {
+            products.Add(new Product
+            {
+                Id = "6214cd9ff357c214a3f3e0b6",
+                Name = "In Memory Pokeball",
+                Description = "This is the test ball",
+                Price = 202M,
+                ImageUrl = "testball.img"
+
+            });
+            products.Add(new Product
+            {
+                Id = "",
+                Name = "In Memory Potion",
+                Description = "This is the test potion",
+                Price = 302M,
+                ImageUrl = "testPotion.jpg"
+
+            });
+        }
+
+        public async Task CreateAsync(Product newProduct)
+        {
+            products.Add(newProduct);
+        }
+
+        public async Task<IEnumerable<Product>> GetAsync()
+        {
+            return products;
+        }
+
+        public async Task<Product?> GetAsync(string id)
+        {
+            return products.FirstOrDefault(x => x.Id == id);
+        }
+
+        public async Task<bool> RemoveAsync(string id)
+        {
+            var prod = products.FirstOrDefault(x => x.Id == id);
+            if (prod != null)
+            {
+                products.Remove(prod);
+                return true;
+            }
+            return false;
+            
+        }
+
+        public async Task<bool> UpdateAsync(string id, Product updateProduct)
+        {
+            var prod = products.FirstOrDefault(x => x.Id == id);
+            if (prod != null)
+            {
+                prod.Name = updateProduct.Name;
+                prod.Description = updateProduct.Description;
+                prod.Price = updateProduct.Price;
+                prod.ImageUrl = updateProduct.ImageUrl;
+
+                return true;
+            }
+           return false;
+        }
+    }
+}
