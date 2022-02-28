939998603538ed8227410fb390f4b15eceadb338
Paul Keating <palisar@hotmail.co.uk>
2022-02-28 18:29:59 +0000

    Added front end project to call api using HttpClientFactory

---
+++ PokeMart.HttpClientDemo.BlazorServ/Models/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace PokeMart.HttpClientDemo.BlazorServ.Models
+{
+    public class ProductDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+}
