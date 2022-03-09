39065134059a00b8d0e130234b597cb1005dc36e
Paul Keating <palisar@hotmail.co.uk>
2022-03-09 17:13:47 +0000

    Added Data Annotations

--- PokéMart.API/Models/AddressDetails.cs
+++ PokéMart.API/Models/AddressDetails.cs
@@ -1,14 +1,24 @@
-﻿namespace PokéMart.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokéMart.API.Models;
 
 public class AddressDetails
 {
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string AddressLine1 { get; set; }
+    
     public string AddressLine2 { get; set; }
     public string AddressLine3 { get; set; }
+    [Required]
     public string Country { get; set; }
+    
     public string State { get; set; }
+    
     public string County { get; set; }
+    [Required]
     public string PostCode { get; set; }
+    [Required]
     public ContactDetails ContactDetails { get; set; }
 }
