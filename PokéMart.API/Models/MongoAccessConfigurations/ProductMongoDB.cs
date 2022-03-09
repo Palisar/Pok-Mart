9a6d05a25bfd78a34eddaf13c8bb99f3a21d7adf
Paul Keating <palisar@hotmail.co.uk>
2022-03-09 16:13:00 +0000

    renamed Mongo Config folder added configuration for connecting to the Order Database

---
+++ PokéMart.API/Models/MongoAccessConfigurations/ProductMongoDB.cs
@@ -0,0 +1,9 @@
+﻿namespace PokéMart.API.Models.MongoAccessConfigurations
+{
+    public class ProductMongoDB
+    {
+        public string connectionString { get; set; } = null!;
+        public string databaseName { get; set; } = null!;
+        public string collectionName { get; set; } = null!;
+    }
+}
